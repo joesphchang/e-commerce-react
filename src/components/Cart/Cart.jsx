a7c0a886c988f06f6870fe34c6d273f834cf41d4
joesphchang <joesphschang@gmail.com>
2023-05-02 14:56:58 -0500

    successfully added item to cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,38 +1,41 @@
 import React from 'react'
 import './Cart.scss';
 import { DeleteForever } from '@mui/icons-material';
+import { useSelector } from 'react-redux';
 
 const Cart = () => {
 
-    const data = [
-        {
-            id: 1,
-            img: 'https://i.imgur.com/4AkVACK.jpg',
-            title: 'T-Shirt',
-            isNew: true,
-            oldPrice: 30,
-            price: 22,
-            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum fusce ut placerat orci nulla pellentesque dignissim enim. Diam quis enim lobortis scelerisque fermentum dui faucibus. Erat nam at lectus urna duis convallis.'
-        },
-        {
-            id: 2,
-            img: 'https://i.imgur.com/jlVNPGQ.jpg',
-            title: 'Jeans',
-            isNew: true,
-            oldPrice: 65,
-            price: 50,
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum fusce ut placerat orci nulla pellentesque dignissim enim. Diam quis enim lobortis scelerisque fermentum dui faucibus. Erat nam at lectus urna duis convallis."
-        }
-    ]
+    // const data = [
+    //     {
+    //         id: 1,
+    //         img: 'https://i.imgur.com/4AkVACK.jpg',
+    //         title: 'T-Shirt',
+    //         isNew: true,
+    //         oldPrice: 30,
+    //         price: 22,
+    //         desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum fusce ut placerat orci nulla pellentesque dignissim enim. Diam quis enim lobortis scelerisque fermentum dui faucibus. Erat nam at lectus urna duis convallis.'
+    //     },
+    //     {
+    //         id: 2,
+    //         img: 'https://i.imgur.com/jlVNPGQ.jpg',
+    //         title: 'Jeans',
+    //         isNew: true,
+    //         oldPrice: 65,
+    //         price: 50,
+    //         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum fusce ut placerat orci nulla pellentesque dignissim enim. Diam quis enim lobortis scelerisque fermentum dui faucibus. Erat nam at lectus urna duis convallis."
+    //     }
+    // ]
+
+    const products = useSelector(state => state.cart.products)
 
   return (
     <div className='cart'>
         <h1>Title</h1>
-        {data?.map(item => (
-            <div className="item" key={data.id}>
-                <img src={item.img} alt='' />
+        {products?.map(item => (
+            <div className="item" key={item.id}>
+                <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt='' />
                 <div className="details">
-                    <h1>{data.title}</h1>
+                    <h1>{item.title}</h1>
                     <p>{item.desc?.substring(0, 100)}</p>
                     <div className="price">
                         1 x ${item.price}
--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -5,11 +5,14 @@ import { FavoriteBorder } from '@mui/icons-material';
 import { Balance } from '@mui/icons-material';
 import useFetch from '../../hooks/useFetch';
 import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../../redux/cartReducer';
 
 const Product = () => {
   const id = useParams().id;
   const [selectedImg, setSelectedImg] = useState("image");
   const [quantity, setQuantity] = useState(1);
+  const dispatch = useDispatch();
 
   const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
   return (
@@ -38,7 +41,14 @@ const Product = () => {
           {quantity}
           <button onClick={() => setQuantity(prev=> prev + 1)} >+</button>
         </div>
-        <button className="add">
+        <button className="add" onClick={() => dispatch(addToCart({
+          id: data.id,
+          title: data.attributes.title,
+          desc: data.attributes.desc,
+          price: data.attributes.price,
+          img: data.attributes.image.data.attributes.url,
+          quantity,
+        }))}>
           <AddShoppingCart /> ADD TO CART
         </button>
         <div className="link">
